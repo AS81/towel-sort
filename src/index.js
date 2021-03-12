45951c86ce22cc8a39fb1aeae1ded0ee87762547
AS81 <sanloginchik@gmail.com>
2021-03-12 20:25:45 +0300

    feat: task complit if matrix is not empty

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
+    console.log(`matrix - ${matrix}`);
     let rez = [];
     matrix.forEach(function (item, i) {
-        i % 2 === 0 ? rez.concat(item) : rez.concat(item.reverse());
+        i % 2 === 0
+            ? (rez = rez.concat(item))
+            : (rez = rez.concat(item.reverse()));
     });
     return rez;
 };
