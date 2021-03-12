04d3b69a74fe3bf81debc5ae1d6a9e128ec7df14
AS81 <sanloginchik@gmail.com>
2021-03-12 20:45:19 +0300

    feat: add case if matrix is empty; task complit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    console.log(`matrix - ${matrix}`);
-    let rez = [];
-    matrix.forEach(function (item, i) {
-        i % 2 === 0
-            ? (rez = rez.concat(item))
-            : (rez = rez.concat(item.reverse()));
-    });
-    return rez;
+    let result = [];
+    if (matrix === undefined || matrix.length === 0) {
+        return result;
+    } else {
+        matrix.forEach(function (item, i) {
+            i % 2 === 0
+                ? (result = result.concat(item))
+                : (result = result.concat(item.reverse()));
+        });
+        return result;
+    }
 };
